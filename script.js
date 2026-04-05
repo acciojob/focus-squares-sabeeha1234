396ef603a40e7cebc763a47858a436d858630904
sabeeha shaik <142984283+sabeeha1234@users.noreply.github.com>
2026-04-05 11:17:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll(".square")
+
+        for(let square of squares){
+            square.addEventListener("mouseover",(e)=>addBgColor(e))
+
+            square.addEventListener("mouseout",(e)=>addDefaultColor(e))
+
+
+        }
+        //add color
+        function addBgColor(e){
+            // e.target.style.backgroundColor="blue"
+            let selectedBox = e.target.id
+            for(let i=0;i<squares.length;i++){
+                if(squares[i].id!=selectedBox){
+                    squares[i].style.backgroundColor="#6F4E37"
+
+                }
+            }
+
+
+
+        }
+        //add default
+        function addDefaultColor(e){
+            // e.target.style.backgroundColor="aquamarine"
+            let selectedBox=e.target
+               for(let i=0;i<squares.length;i++){
+                if(squares[i].id!=selectedBox){
+                    squares[i].style.backgroundColor="#E6E6FA"
+
+                }
+            }
+
+        }
